2712a2a5c107228a9047971e55c20e5f0849f586
jensh007 <j.huebel@sap.com>
2023-04-26 08:46:28 +0200

    minor fixes in start scripts

--- create-cert.sh
+++ create-cert.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # Linux:  FQDN_HOST=`hostname --fqdn`
-# Macos:  ip=ip=`ipconfig getifaddr en0`
+# Macos:  ip=`ipconfig getifaddr en0`
 #         FQDN_HOST=`dig -x $ip +short | head -c-2`
-# manual: export FQDN_HOST=mymachine.fritz.box
+# manual: export FQDN_HOST=`hostname`.fritz.box
 openssl req -newkey rsa:4096 -nodes -sha256 -keyout certs/ociregistry.key -addext "subjectAltName = DNS:${FQDN_HOST}" -x509 -days 365 -out certs/ociregistry.crt -subj "/C=DE/ST=Baden-Wuertemberg/L=Walldorf/O=SAP/OU=ocm/CN=${FQDN_HOST}"
