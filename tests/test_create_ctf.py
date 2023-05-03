f7914ae223e3f7ca4984e1af81195adf552801fe
jensh007 <j.huebel@sap.com>
2023-05-03 16:07:48 +0200

    extended cd validation in CTF

--- tests/test_create_ctf.py
+++ tests/test_create_ctf.py
@@ -1,7 +1,14 @@
 from pathlib import Path
+import json
+import pprint
 import shutil
 import textwrap
 import pytest
+import tarfile
+
+import gci.componentmodel as cm
+import oci.model as om
+import yaml
 
 import ocmcli as ocm
 from ocm_fixture import ctx, OcmTestContext
@@ -15,6 +22,70 @@ def get_root_dir() -> Path:
     path = Path(__file__)
     return path.parent.parent.absolute()
 
+
+def find_component_descriptor(ctf_dir: Path) -> cm.ComponentDescriptor:
+    index_file = ctf_dir / 'artifact-index.json'
+    with open(index_file) as f:
+        ind = json.load(f)
+    digest:str = ind['artifacts'][0]['digest']
+    manifest_file = ctf_dir / 'blobs' / digest.replace('sha256:', 'sha256.')
+    with open(manifest_file) as f:
+        oci_manifest = json.load(f)
+    assert oci_manifest['mediaType'] == 'application/vnd.oci.image.manifest.v1+json'
+    assert oci_manifest['layers'][0]['mediaType'] == 'application/vnd.ocm.software.component-descriptor.v2+yaml+tar'
+    cd_digest = oci_manifest['layers'][0]['digest']
+    cd_tar_file =  ctf_dir / 'blobs' / cd_digest.replace('sha256:', 'sha256.')
+    extract_dir = ctf_dir.parent / 'extracted'
+    with tarfile.open(cd_tar_file, 'r') as tar:
+        name = tar.next()
+        print(f'extracting file {name}')
+        tar.extractall(extract_dir)
+    cd_file = extract_dir / name.name
+    cd_file.chmod(0o644)
+    with open(cd_file) as f:
+        cd_dict = yaml.safe_load(f)
+    cd = cm.ComponentDescriptor.from_dict(cd_dict, cm.ValidationMode.NONE)
+    return cd
+
+
+def verify_component_descriptor(cd: cm.ComponentDescriptor):
+    assert cd.meta.schemaVersion == cm.SchemaVersion.V2
+    assert cd.component.name == comp_name
+    assert cd.component.version  == comp_vers
+    assert len(cd.component.resources) == 2
+    assert len(cd.component.sources) == 1
+    chart = cd.component.resources[0]
+    assert chart.name == 'chart'
+    assert chart.type == cm.ArtefactType.HELM_CHART
+    assert chart.relation == cm.ResourceRelation.LOCAL
+    assert chart.version == '1.0.0'
+    assert chart.access.type == cm.AccessType.LOCAL_BLOB
+    print(f'{type(chart.access)=}')
+    # does not work at this time:
+    # assert type(chart.access) == cm.LocalBlobAccess
+    # assert chart.access.localReference.startswith('sha256:')
+    # assert chart.access.mediaType == 'application/vnd.oci.image.manifest.v1+tar+gzip'
+    # assert chart.access.referenceName == f'{provider}/echo/echoserver:0.1.0'
+    image = cd.component.resources[1]
+    assert image.name == 'image'
+    assert image.type == cm.ArtefactType.OCI_IMAGE
+    assert image.version == '1.10'
+    assert image.relation == cm.ResourceRelation.EXTERNAL
+    assert type(image.access) == cm.OciAccess #  cm.AccessType.OCI_REGISTRY
+    assert image.access.imageReference == 'gcr.io/google_containers/echoserver:1.10'
+    assert len(image.labels) == 1
+    assert image.labels[0].name == 'mylabel'
+    assert image.labels[0].value == 'Hello Label'
+    assert len(cd.component.sources) == 1
+    source = cd.component.sources[0]
+    assert source.name == 'source'
+    assert source.type == 'filesystem'
+    assert source.version == comp_vers
+    assert source.access.type == cm.AccessType.GITHUB
+    assert type(source.access) == cm.GithubAccess
+    assert source.access.commit == 'e39625d6e919d33267da4778a1842670ce2bbf77'
+    assert source.access.repoUrl == 'github.com/open-component-model/ocm'
+
 def test_ctf_from_ca(ctx: OcmTestContext):
     # create an image with docker mime types and store it in oci registry
     gen_dir = get_root_dir() / 'gen'
@@ -22,7 +93,7 @@ def test_ctf_from_ca(ctx: OcmTestContext):
     test_dir = gen_dir / 'test_ctf_from_ca'
     sources_yaml = textwrap.dedent(f'''\
         name: source
-        type: filesytem
+        type: filesystem
         access:
             type: github
             repoUrl: github.com/open-component-model/ocm
@@ -82,8 +153,9 @@ def test_ctf_from_ca(ctx: OcmTestContext):
     assert count == 1
     assert cd.exists()
 
-    blob_dir = test_dir / 'ctf' / 'blobs'
-    index_file = test_dir / 'ctf' / 'artifact-index.json'
+    ctf_dir = test_dir / 'ctf'
+    blob_dir = ctf_dir / 'blobs'
+    index_file = ctf_dir / 'artifact-index.json'
     assert blob_dir.exists()
     assert index_file.exists()
 
@@ -91,3 +163,8 @@ def test_ctf_from_ca(ctx: OcmTestContext):
     for child in blob_dir.iterdir():
         count += 1
         assert child.name.startswith('sha256.')
+
+    # retrieve component descriptor
+    cd = find_component_descriptor(ctf_dir)
+    assert cd
+    verify_component_descriptor(cd)
